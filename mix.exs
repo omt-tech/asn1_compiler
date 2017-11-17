44644858f811c44f1b23f1c336fc3d7ba62ebfcc
Michał Muskała <michal@muskala.eu>
2017-11-17 10:53:57 +0100

    Prepare for release

--- mix.exs
+++ mix.exs
@@ -1,24 +1,48 @@
 defmodule Asn1Compiler.MixProject do
   use Mix.Project
 
-  def project do
+  @version "0.1.0"
+
+  def project() do
     [
       app: :asn1_compiler,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.4",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: """
+      A mix compiler for the ASN.1 format leveraging Erlang's `:asn1_ct`.
+      """,
+      package: package(),
+      docs: docs()
     ]
   end
 
-  def application do
+  def application() do
     [
       extra_applications: []
     ]
   end
 
-  defp deps do
+  defp deps() do
     [
+      {:ex_doc, "~> 0.14", only: :dev}
+    ]
+  end
+
+  defp package() do
+    [
+      licences: ["MIT"],
+      maintainers: ["Michał Muskała"],
+      links: %{"GitHub" => "https://github.com/omt-tech/rest_auth"}
+    ]
+  end
+
+  defp docs() do
+    [
+      main: "RestAuth",
+      source_ref: "v#{@version}",
+      source_url: "https://github.com/omt-tech/rest_auth"
     ]
   end
 end
